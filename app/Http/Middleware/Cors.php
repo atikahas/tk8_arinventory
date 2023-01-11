4b84b72d76f276bff27f612e258b3b6fa1fe9b5c
atikahas <45572226+atikahas@users.noreply.github.com>
2023-01-11 17:18:21 +0800

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,7 @@ class Cors
         header('Access-Control-Allow-Origin:  *');
         // header('Access-Control-Allow-Origin:  http://localhost:4200');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-        header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
+        header('Access-Control-Allow-Methods:  POST, GET');
 
         return $next($request);
     }
